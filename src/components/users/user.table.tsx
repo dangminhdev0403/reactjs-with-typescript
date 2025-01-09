eef2fb5aab644ee26391a9a103ab410b65e035cb
Your Name <you@example.com>
2025-01-10 06:40:01 +0700

    scss in react

--- src/components/users/user.table.tsx
+++ src/components/users/user.table.tsx
@@ -1,8 +1,40 @@
-//import users.css 
-import '../../styles/users.css';
+//import users.css
+import { useEffect, useState } from "react";
+import "../../styles/users.css";
 
-
+interface IUser{
+  id:number;
+  name:string;
+  email:string;
+  gender:string;
+}
 const UserTable = () => {
+  const [listUsers, setListUsers] = useState([]);
+
+  useEffect(() => {
+    console.log("check useEffect");
+
+    getData();
+  }, []);
+
+
+  const getData = async () => {
+    const access_token =
+      "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJwZXJtaXNzaW9uIjpbIlJPTEVfQURNSU5fQ1JFQVRFIiwiUk9MRV9BRE1JTl9VUERBVEUiXSwiZXhwIjoxNzQ1MDk1ODExLCJpYXQiOjE3MzY0NTU4MTEsInVzZXIiOnsiaWQiOjEsIm5hbWUiOiJJJ20gc3VwZXIgYWRtaW4iLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSJ9fQ.FsebO4mxxFWV2W4WEhc7q_WC0RS0MXgGrx8kkKn2Isfk5fgshn4__YwLXBFozxOZi8Gu0nOd2xpBIO6mCZcRoA";
+    const res = await fetch("http://127.0.0.1:8080/api/v1/users", {
+      method: "GET",
+      //fetch with beartoken
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "COntent-Type": "application/json",
+      },
+    });
+
+    const d = await res.json();
+
+    setListUsers(d.data.result);
+  };
+
   return (
     <div>
       <h2>Bảng với 2 cột và dữ liệu mẫu</h2>
@@ -10,27 +42,21 @@ const UserTable = () => {
       <table>
         <thead>
           <tr>
-            <th>Cột 1</th>
-            <th>Cột 2</th>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>gender</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Dữ liệu 1</td>
-            <td>Dữ liệu A</td>
-          </tr>
-          <tr>
-            <td>Dữ liệu 2</td>
-            <td>Dữ liệu B</td>
-          </tr>
-          <tr>
-            <td>Dữ liệu 3</td>
-            <td>Dữ liệu C</td>
-          </tr>
-          <tr>
-            <td>Dữ liệu 4</td>
-            <td>Dữ liệu D</td>
-          </tr>
+          {listUsers.map((item: IUser) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.gender}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,74 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-// import './index.css'
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App";
+import "./App.scss";
 import UserPage from "./screens/user.page";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+  {
+    label: <Link to="/"> Home </Link>,
+    key: "home",
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to="/users"> Manager User </Link>,
+    key: "users",
+    icon: <UserOutlined />,
+  },
+];
+
+const Header = () => {
+  const [current, setCurrent] = useState("home");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+};
+
+const LayoutAdmin = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+
+    element: <LayoutAdmin />,
+    children: [
+      { index: true, element: <App /> },
+      {
+        path: "users",
+        element: <UserPage />,
+      },
+    ],
   },
-  {
-    path:"/users",
-    element: <UserPage/>,
-  }
 ]);
 
 createRoot(document.getElementById("root")!).render(
